a0f888f4fa58af6fbe18e710f8d047994629c961
Patricia-LF <patricia_lf_86@hotmail.com>
2026-08-17 18:54:28 +0200

    Updated CV

--- hero.php
+++ hero.php
@@ -11,7 +11,7 @@
                 <p class="intro">I'm actively looking for a LIA internship for the period Nov 23, 2026 – May 28, 2027.
                 </p>
                 <div class="cta">
-                    <a href="CV 260615.pdf" download target="_blank" class="btn-download">
+                    <a href="CV Patricia Loayza Frykberg 260816.pdf" download target="_blank" class="btn-download">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                         </svg>
